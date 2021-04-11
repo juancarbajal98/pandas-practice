24cb98ab2184ef48892117cb90769f9ad436560c
Juan Carbajal <juancarbajal98@gmail.com>
2021-04-10 23:31:54 -0700

    Movie instances created and filled with CSV data

--- main.py
+++ main.py
@@ -1,5 +1,33 @@
 import pandas as pd
+class Movie: 
+    def __init__(self, title, date, genre, rating, gross, inflated_gross):
+        self.title = title
+        self.date = date
+        self.genre = genre 
+        self.rating = rating 
+        self.gross = gross
+        self.inflated_gross = inflated_gross
 
-df = pd.read_csv('disney_movies_total_gross.csv')
+def print_movies(start, finish, movies):
+    for i in range(start, finish):
+        print(movies[i].title, movies[i].date, movies[i].genre, movies[i].rating, movies[i].gross, movies[i].inflated_gross)    
+            
+movies = []
+titles = pd.read_csv('disney_movies_total_gross.csv', usecols=[0])
+dates = pd.read_csv('disney_movies_total_gross.csv', usecols=[1])
+genres = pd.read_csv('disney_movies_total_gross.csv', usecols=[2])
+ratings = pd.read_csv('disney_movies_total_gross.csv', usecols=[3])
+gross = pd.read_csv('disney_movies_total_gross.csv', usecols=[4])
+inflated_gross = pd.read_csv('disney_movies_total_gross.csv', usecols=[5])
 
-print(df.to_string())
+for j in range(len(titles)):
+    movies.append(Movie(
+        titles['movie_title'][j],
+        dates['release_date'][j],
+        genres['genre'][j],
+        ratings['mpaa_rating'][j],
+        gross['total_gross'][j],
+        inflated_gross['inflation_adjusted_gross'][j]
+    ))
+
+print_movies(0,20,movies)
